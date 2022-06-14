9cd06a979710bb1cd638dc24d92a7320c68411a9
Gadise Mulat <gtmulat@gmail.com>
2022-06-13 19:09:18 -0500

    Organize folders

---
+++ static/js/app.js
@@ -0,0 +1,34 @@
+// import the data from data.js
+const tableData = data
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody")
+
+// Simple JavaScript console.log statement
+function printHello();
+
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+  }
+
+// Original addition function
+function addition(a, b) {
+    return a + b;
+  }
+
+// Converted to an arrow function
+addition = (a, b) => a + b;
+
+function buildTable(data) {
+    tbody.html("");
+  }
+
+data.forEach((dataRow) => {
+
+});
+
+let row = tbody.append("tr");
+
+Object.values(dataRow).forEach((val) => {
+});
