975f51c4ebbc0b025b536f720089413bbe8b451c
Gadise Mulat <gtmulat@gmail.com>
2022-06-13 21:21:59 -0500

    added image

--- static/js/app.js
+++ static/js/app.js
@@ -4,35 +4,25 @@ const tableData = data
 // Reference the HTML table using d3
 var tbody = d3.select("tbody")
 
-// Simple JavaScript console.log statement
-function printHello();
-
-// Simple JavaScript console.log statement
-function printHello() {
-    console.log("Hello there!");
-  }
-
-// Original addition function
-function addition(a, b) {
-    return a + b;
-  }
-
-// Converted to an arrow function
-addition = (a, b) => a + b;
-
 function buildTable(data) {
-    tbody.html("");
-  }
+  // First, clear out any existing data
+  tbody.html("");
 
-data.forEach((dataRow) => {
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
 
-});
-
-let row = tbody.append("tr");
-
-Object.values(dataRow).forEach((val) => {
-});
-
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+      cell.text(val);
+      }
+    );
+  });
+}
 // Add date & time
 function handleClick() {
   // Grab the datetime value from the filter
